bdd5b2d0c0fa18315af2a3f97c6cba7ecf047699
Sakthi20I239 <120664299+Sakthi20I239@users.noreply.github.com>
2023-02-17 14:21:51 +0530

    guassian

---
+++ guassian.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import random
+def gaussian_func(image):
+    image = cv2.imread(image)
+    PEAK=1
+    return np.random.normal(image / 30 * PEAK) / PEAK * 100
+# image = cv2.imread("pic1.jpeg")  # need a rescale to be more realistic
+
+cv2.imwrite('gaussian.jpeg',gaussian_func("./images/image-5.jpg"))
+ 
