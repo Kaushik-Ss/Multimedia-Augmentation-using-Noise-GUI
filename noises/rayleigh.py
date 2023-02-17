d2bcb6890e0a4290d2efd4a70d4c1890aa3449c8
Sakthi20I239 <120664299+Sakthi20I239@users.noreply.github.com>
2023-02-17 15:42:43 +0530

    change

--- noises/rayleigh.py
+++ noises/rayleigh.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 import random
-def poisson_func(image):
+def rayleigh_func(image):
     image = cv2.imread(image)
     PEAK=1
-    return np.random.rayleigh(image / 255.0 * PEAK) / PEAK * 255
+    return np.random.(image / 255.0 * PEAK) / PEAK * 255
 # image = cv2.imread("pic1.jpeg")  # need a rescale to be more realistic
-cv2.imwrite('rayleigh.jpeg',poisson_func("./images/pic1.jpeg"))
+cv2.imwrite('rayleigh.jpeg',rayleigh_func("./images/pic1.jpeg"))
