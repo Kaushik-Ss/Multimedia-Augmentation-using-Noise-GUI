0cfd1e9449e3ad81e597e86faef96d1b216e039e
Fredrick John <106023592+fredrickjohn10@users.noreply.github.com>
2023-02-17 14:36:02 +0530

    Uniform noise
    Wand

---
+++ noises/uniform.py
@@ -0,0 +1,10 @@
+# Import Image from wand.image module
+from wand.image import Image
+  
+# Read image using Image() function
+def uniformnoise(imagelocation,attenuaterange):
+    with Image(filename = imagelocation) as img:
+        img.noise("uniform", attenuate = attenuaterange)
+        # img.save(filename ="uniformnoise.jpeg")
+
+uniformnoise(r"C:\Users\fredd\Documents\GitHub\Prism-gui-image-augment\images\image-28.jpg",0.9)
